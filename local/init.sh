a06ae4ccd72eb7da128998ffeaaaa741f3ebc437
obitech <obitech@users.noreply.github.com>
2018-09-22 16:12:28 +0200

    updated local deployments for v1.1

--- local/init.sh
+++ local/init.sh
@@ -1,8 +1,16 @@
 
 #!/usr/bin/env bash
 
-JIBJIB_MODEL_PATH=$(pwd)/data/query/model
-JIBJIB_PICKLE_PATH=$(pwd)/data/query/pickle
+JIBJIB_MODEL_PATH="$(pwd)/data/query/model"
+JIBJIB_PICKLE_PATH="$(pwd)/data/query/pickle"
+
+MODEL="https://s3-eu-west-1.amazonaws.com/jibjib/model/jibjib_model_serving_145.tgz"
+
+BIRD_ID_URL="https://s3-eu-west-1.amazonaws.com/jibjib/pickle/bird_id_map_1.0.0.pickle"
+BIRD_ID_DEST="${JIBJIB_PICKLE_PATH}/bird_id_map_1.0.0.pickle"
+
+TRAIN_ID_URL="https://s3-eu-west-1.amazonaws.com/jibjib/pickle/train_id_list_1.1.0.pickle"
+TRAIN_ID_DEST="${JIBJIB_PICKLE_PATH}/train_id_list_1.1.0.pickle"
 
 # Create data folders, if necessary
 if [ ! -d "${JIBJIB_MODEL_PATH}" ]; then
@@ -13,9 +21,9 @@ if [ ! -d "${JIBJIB_PICKLE_PATH}" ]; then
 fi
 
 echo "Fetching model"
-curl https://s3-eu-west-1.amazonaws.com/jibjib/model/jibjib_model_serving_v1.tgz | \
-	tar -xvz -C ${JIBJIB_MODEL_PATH}
+wget ${MODEL} -O - | tar -xvz -C ${JIBJIB_MODEL_PATH}
 
 echo "Fetching pickles"
-curl https://s3-eu-west-1.amazonaws.com/jibjib/pickle/mapping_pickles.tgz | \
-	tar -xvz -C ${JIBJIB_PICKLE_PATH}
+wget ${BIRD_ID_URL} -O ${BIRD_ID_DEST}
+
+wget ${TRAIN_ID_URL} -O ${TRAIN_ID_DEST}
